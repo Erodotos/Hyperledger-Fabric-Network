21b023574466a9761f7196936e272932b0c31b0a
stavroulla <erodotos@stavroulla.in.cs.ucy.ac.cy>
2020-09-21 08:16:13 +0300

    Rolled back rawTwo from Contract API to Shim API; resolves library problems.

--- chaincode/src/rawTwo/contract_rawTwo.go
+++ chaincode/src/rawTwo/contract_rawTwo.go
@@ -3,71 +3,53 @@ package main
 import (
 	"fmt"
 
-	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	pb "github.com/hyperledger/fabric-protos-go/peer"
 )
 
-type SmartContract struct {
-	contractapi.Contract
+// SimpleChaincode example simple Chaincode implementation
+type SimpleChaincode struct {
 }
 
-/********	Note: Function signatures and structs will change accordingly during implementation.  ********/
-
-// This is the struct that will receive the input data batch.
-// The datatype of each data entry of the input batch is yet unclear.
-type DataBatch struct {
-	Batch []interface{}
-}
-
-// Writes a data batch to the ledger.
-// It creates a ledger entry for every data batch. Every batch gets its own key, in the form of
-// <deviceID>_<minTimestamp in inputu data batch>, to enable the use of the getStateByRangeWithPagination
-// function effectively.
-func (sc *SmartContract) WriteBatch(ctx contractapi.TransactionContextInterface, batch string) error {
-	/*
-		var data DataBatch
-		err = json.Unmarshal([]byte(batch), &data)
-		if err != nil {
-			return nil, errors.New("DataBatch unmarshal failed!") //err
-		}
-	*/
-	return nil
-}
-
-// Retrieves the ledger entries which correspond to the requested range/time interval, for a single device.
-// It uses the getStateByRangeWithPagination(startKey, endKey, pageSize, bookmark) to retrieve the needed
-// key-range, and then discards the unnecessary ledger entries (i.e. out of requested time interval),
-// and returns the rest of the data.
-func (sc *SmartContract) QueryDeviceRange(ctx contractapi.TransactionContextInterface) error {
-	return nil
-}
-
-// Registers a new device on the ledger.
-// It creates a composite key ("location~deviceID") which is used as an index for location-based querying
-// purposes. (Here "location-based" refers to the location that the device registers at.)
-func (sc *SmartContract) RegisterDevice(ctx contractapi.TransactionContextInterface) error {
-	return nil
-}
-
-// Retrieves the ledger entries which correspond to the requested range/time interval, for all the devices in the
-// specified location.
-// It uses the "location~deviceID" composite keys to get the identifier of all devices in the specified
-// location, and then works similarly to the QueryDeviceRange function to retrieve the correct data for
-// each of those devices. It returns the results of all devices in a single struct; grouped per device.
-func (sc *SmartContract) QueryLocationRange(ctx contractapi.TransactionContextInterface) error {
-	return nil
-}
-
+// ===================================================================================
+// Main
+// ===================================================================================
 func main() {
-
-	chaincode, err := contractapi.NewChaincode(new(SmartContract))
-
+	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
-		fmt.Printf("Error creating chaincode: %s", err.Error())
-		return
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
+
+// Init initializes chaincode
+// ===========================
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
+}
+
+// Invoke - Our entry point for Invocations
+// ========================================
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	function, args := stub.GetFunctionAndParameters()
+	fmt.Println("invoke is running " + function)
+
+	// Handle different functions
+	if function == "WriteBatch" {
+		return t.WriteBatch(stub, args)
+	} else if function == "QueryRangeWithPagination" {
+		return t.QueryRangeWithPagination(stub, args)
 	}
 
-	if err := chaincode.Start(); err != nil {
-		fmt.Printf("Error starting chaincode: %s", err.Error())
-	}
+	fmt.Println("invoke did not find func: " + function) //error
+	return shim.Error("Received unknown function invocation")
+}
 
+func (t *SimpleChaincode) WriteBatch(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+
+	return shim.Success([]byte{0x00})
+}
+
+func (t *SimpleChaincode) QueryRangeWithPagination(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+
+	return shim.Success([]byte{0x00})
 }
--- chaincode/src/rawTwo/go.mod
+++ chaincode/src/rawTwo/go.mod
@@ -1,8 +1,9 @@
-module github.com/hyperledger/chaincode/documents/go
+module contract_rawTwo
 
 go 1.13
 
 require (
-	github.com/hyperledger/fabric-contract-api-go v1.0.0
-	github.com/influxdata/influxdb-client-go v1.3.0
+	github.com/hyperledger/fabric-chaincode-go v0.0.0-20200728190242-9b3ae92d8664
+	github.com/hyperledger/fabric-protos-go v0.0.0-20200917184523-71c4060efc42
+	github.com/influxdata/influxdb-client-go v1.4.0 // indirect
 )
